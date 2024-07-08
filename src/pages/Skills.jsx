4a4c35205e993cbc5588008b8b0403ce105046c3
Shameen <shameenmunasinghe@gmail.com>
2024-07-09 00:27:32 +0530

    update work and skill pages

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,12 +9,15 @@ import Node from "../assets/node.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
 import Mongo from "../assets/mongo.png";
+import Bootstrap from "../assets/Bootstrap2.png";
+import NETCore from "../assets/ASPNET.png";
+import JQuery from "../assets/JQuery.png";
 
 const Skills = () => {
   return (
-    <div className="w-full sm:h-screen bg-[#0a192f] text-gray-300">
+    <div className="w-full min-h-screen bg-[#0a192f] text-gray-300">
       <Navbar />
-      <div className="max-w-[1000px] mx-auto flex flex-col justify-center h-full w-full">
+      <div className="max-w-[1000px] mx-auto flex flex-col justify-center h-full w-full p-4 pt-20 pb-10">
         <div className="mt-20 ml-5 sm:ml-0 sm:text-left sm:mt-0">
           <p className="inline text-4xl font-bold border-b-4 border-pink-600 ">
             Skills
@@ -22,37 +25,53 @@ const Skills = () => {
           <p className="py-4">These are the technologies I've worked with</p>
         </div>
         <div className="grid w-full grid-cols-2 gap-4 p-5 text-center sm:grid-cols-4">
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
             <p className="my-4">CSS</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={JavaScript} alt="CSS icon" />
             <p className="my-4">JavaScript</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
             <p className="my-4">React</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
             <p className="my-4">Tailwind</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={Node} alt="Node icon" />
             <p className="my-4">Node</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={Mongo} alt="MongoDB icon" />
             <p className="my-4">MongoDB</p>
           </div>
-          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
             <img className="w-20 mx-auto" src={GitHub} alt="Github icon" />
             <p className="my-4">Github</p>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
+            <img className="w-20 mx-auto" src={Bootstrap} alt="Github icon" />
+            <p className="my-4">Bootstrap</p>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
+            <img className="w-20 mx-auto" src={JQuery} alt="Github icon" />
+            <p className="my-4">jQuery</p>
+          </div>
+          <div className="shadow-lg shadow-[#040c16] hover:scale-110 duration-500 pt-5">
+            <img
+              className="w-20 mx-auto rounded-full"
+              src={NETCore}
+              alt="Github icon"
+            />
+            <p className="my-4">ASP.NET Core</p>
           </div>
         </div>
       </div>
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -3,31 +3,95 @@ import Navbar from "../component/Navbar";
 import Home5 from "../assets/Home5.png";
 import Home6 from "../assets/Home6.png";
 import Home7 from "../assets/Home7.png";
+import Todo_1 from "../assets/Todo_1.png";
+import Todo_2 from "../assets/Todo_2.png";
+import Todo_3 from "../assets/Todo_3.png";
+import SMS2 from "../assets/SMS2.png";
+import SMS3 from "../assets/SMS3.png";
+import SMS4 from "../assets/SMS4.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full sm:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="w-full text-gray-300 bg-[#0a192f] min-h-screen "
+    >
       <Navbar />
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 pt-20 pb-10 flex flex-col justify-center w-full h-full ">
         <div className="">
           <p className="inline text-4xl font-bold text-gray-300 border-b-4 border-pink-600">
             Work
           </p>
           <p className="py-4">Check out some of my recent work</p>
         </div>
-        <p className="pb-4 font-semibold">Travel Guide Project</p>
 
-        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 ">
-          <img src={Home5} className="shadow-xl shadow-[#040c16] " />
-          <img src={Home6} className="shadow-xl shadow-[#040c16] " />
-          <img src={Home7} className="shadow-xl shadow-[#040c16] " />
+        {/* travel guide project */}
+        <div>
+          <p className="pb-4 font-semibold">Travel Guide Project</p>
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 ">
+            <img src={Home5} className="shadow-xl shadow-[#040c16] " />
+            <img src={Home6} className="shadow-xl shadow-[#040c16] " />
+            <img src={Home7} className="shadow-xl shadow-[#040c16] " />
+          </div>
+          <p className="mt-2 ">
+            Technologies Used: HTML | JavaScript | TailwindCSS | ReactJS |
+            NodeJS | ExpressJS | MongoDB | Github
+          </p>
+          <a
+            href="https://github.com/TourGuide-GroupJ/TourGuide.git"
+            className="text-blue-300 underline"
+          >
+            Project Link
+          </a>
         </div>
-        <a
-          href="https://github.com/TourGuide-GroupJ/TourGuide.git"
-          className=" underline text-blue-200"
-        >
-          Project Link
-        </a>
+
+        {/* Student management system */}
+        <div className="mt-10 ">
+          <p className="pb-4 font-semibold">Student Management System</p>
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 ">
+            <img src={SMS4} className="shadow-xl shadow-[#040c16] " />
+            <img src={SMS3} className="shadow-xl shadow-[#040c16] " />
+            <img src={SMS2} className="shadow-xl shadow-[#040c16] " />
+          </div>
+          <p className="mt-2 ">
+            Developed a Student Management System to manage student information.
+            Key features include viewing, adding, updating, and deleting student
+            details. Only logged-in teachers can modify records, ensuring secure
+            data handling and user authentication.
+          </p>
+          <p>
+            Technologies Used: HTML | JavaScript | Bootstrap | ReactJS | NodeJS
+            | ExpressJS | MongoDB | JWT | Github
+          </p>
+          <a
+            href="https://github.com/Shameen98/Student-Management-System.git"
+            className="text-blue-300 underline"
+          >
+            Project Link
+          </a>
+        </div>
+
+        {/* to do list */}
+        <div className="mt-10 ">
+          <p className="pb-4 font-semibold">TODO List</p>
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 ">
+            <img src={Todo_1} className="shadow-xl shadow-[#040c16] " />
+            <img src={Todo_2} className="shadow-xl shadow-[#040c16] " />
+            <img src={Todo_3} className="shadow-xl shadow-[#040c16] " />
+          </div>
+          <p className="mt-2 ">
+            This TODO list application is a practical and user-friendly tool
+            designed to help manage daily tasks efficiently. Users can easily
+            add, delete, and mark tasks as complete.
+          </p>
+          <p>Technologies Used: HTML | CSS | JavaScript (jQuery)</p>
+          <a
+            href="https://github.com/Shameen98/Todo-List.git"
+            className="text-blue-300 underline"
+          >
+            Project Link
+          </a>
+        </div>
       </div>
     </div>
   );
